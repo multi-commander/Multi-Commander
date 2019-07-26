e6eea5d6a43c7aafa91da1b7e30cf955fb9d089b
Skylark <24936522+Skylark0924@users.noreply.github.com>
2019-07-26 14:38:11 +0800

    Create environment_demo.py

---
+++ demo/environment_demo.py
@@ -0,0 +1,50 @@
+import ray
+ray.init()
+
+import gym
+import numpy as np
+import cityflow
+
+
+@ray.remote
+class GymEnvironment(object):
+	def __init__(self, name):
+		self.env = gym.make(name)
+		self.env.reset()
+
+	def step(self, action):
+		return self.env.step(action)
+
+	def reset(self):
+		self.env.reset()
+
+@ray.remote
+class CityFlowEnv(object):
+	def __init__(self, config_path):
+		self.eng = cityflow.Engine(config_path, thread_num=1)
+
+	def step(self):
+		return self.eng.next_step()
+
+# def main():
+# 	import time
+# 	import matplotlib.pyplot as plt
+
+# 	pong = GymEnvironment.remote('Pong-v0') # this is an actor
+# 	plt.ion()
+# 	while True:
+# 		action = np.random.randint(0, 6)
+# 		res = ray.get( pong.step.remote(action) )
+# 		# print(res)
+# 		plt.imshow(res[0])
+# 		# time.sleep(0.1)
+# 		plt.pause(0.1)
+
+def main():
+	config_path = "./examples/config_1x1.json"
+	cityEnv = CityFlowEnv.remote(config_path)
+	for i in range(10 ** 3):
+		cityEnv.step.remote()
+
+if __name__ == '__main__':
+	main()
